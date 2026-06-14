a42067864d237a527a811d4c65fe0ed4c6a3f270
MagicKoffe <83956712+MagicKoffe@users.noreply.github.com>
2026-06-14 02:38:37 +0100

    jumping + movement relative to cam
    Added basic jumping and movement using the Unity character controller. The movement vector is calculated relative to the camera.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,66 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController characterController;
+    private Vector3 moveInput;
+    private Vector3 velocity;
 
+    [Header("Movement stats")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.8f;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        //assuming we only using the single camera:
+        var camera = Camera.main;
+
+        //camera forward and right vectors:
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 xVector = right * moveInput.x;
+        Vector3 yVector = forward * moveInput.y;
+
+        Vector3 move = xVector + yVector;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Jumping {context.performed} - is grounded: {characterController.isGrounded}");
+        if(context.performed && characterController.isGrounded)
+        {
+            Debug.Log("Jump");
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+      
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {moveInput}");
+    }
+
 }
