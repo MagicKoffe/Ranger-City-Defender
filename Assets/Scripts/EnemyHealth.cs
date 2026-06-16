3e6af26b2ab3fab224315b80d863e8d191e6f341
MagicKoffe <83956712+MagicKoffe@users.noreply.github.com>
2026-06-16 18:13:19 +0100

    Healthbar to enemy
    Enemies now have a healthbar using the world canvas

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,21 +1,32 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] int maxHealth;
-    [SerializeField] int currentHealth;
+    [Header("Stats")]
+    [SerializeField] private int maxHealth;
+    private int currentHealth;
+
+    [Header("Visuals")]
+    [SerializeField] Image healthBar;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        updateHealthBar();
+    }
+
+    private void updateHealthBar()
+    {
+        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
     }
 
     public void TakeDamage(int dmg)
     {
         Debug.Log($"{gameObject.name} took {dmg} damage.");
-
         currentHealth -= dmg;
+        updateHealthBar();
 
         if(currentHealth <= 0)
         {
